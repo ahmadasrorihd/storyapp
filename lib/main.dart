fbf58cff215022301f6bd37ecf06be070b0de35c
Ahmad Asrori <ahmad.asrori@axa.co.id>
2023-10-12 07:05:10 +0700

    on progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
@@ -26,50 +25,53 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   final bool? isLogin;
+  final GoRouter _router;
+
   MyApp({
     Key? key,
     required this.isLogin,
-  }) : super(key: key);
+  })  : _router = GoRouter(
+          routes: [
+            GoRoute(
+              path: "/",
+              name: 'login',
+              builder: (context, state) => const Login(),
+              redirect: (context, state) {
+                if (isLogin == null || false) {
+                  return "/";
+                } else {
+                  return "/list";
+                }
+              },
+            ),
+            GoRoute(
+                path: "/register",
+                name: 'register',
+                builder: (context, state) => const Register()),
+            GoRoute(
+                path: "/list",
+                name: 'list',
+                builder: (context, state) => const ListStory()),
+            GoRoute(
+                path: "/add",
+                name: 'add',
+                builder: (context, state) => const AddStory()),
+            GoRoute(
+                path: "/detail/:storyId",
+                name: 'detail',
+                builder: (context, state) => DetailStory(
+                      storyId: state.pathParameters['storyId']!,
+                    )),
+          ],
+        ),
+        super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
       title: "StoryApp",
-      routerConfig: _router, // Pass context to _router
+      routerConfig: _router,
     );
   }
-
-  final GoRouter _router = GoRouter(
-    // redirect: (context, state) {
-    //   if (isLogin == null || false) {
-    //     return "/";
-    //   } else {
-    //     return "/list";
-    //   }
-    // },
-    routes: [
-      GoRoute(
-          path: "/", name: 'login', builder: (context, state) => const Login()),
-      GoRoute(
-          path: "/register",
-          name: 'register',
-          builder: (context, state) => const Register()),
-      GoRoute(
-          path: "/list",
-          name: 'list',
-          builder: (context, state) => const ListStory()),
-      GoRoute(
-          path: "/add",
-          name: 'add',
-          builder: (context, state) => const AddStory()),
-      GoRoute(
-          path: "/detail/:storyId",
-          name: 'detail',
-          builder: (context, state) => DetailStory(
-                storyId: state.pathParameters['storyId']!,
-              )),
-    ],
-  );
 }
